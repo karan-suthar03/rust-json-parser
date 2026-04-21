9a914e8db6d5232ae8aee972153cf338c6b88e03
karan-suthar03 <karan.suthar.lakha@gmail.com>
2026-04-21 18:52:31 +0530

    value parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
+use std::collections::vec_deque::Iter;
 use std::fmt::Display;
 use std::fs;
 use std::string::String;
 
-#[derive(Debug)]
+#[derive(Debug,Clone)]
 enum Token{
     LeftCurlyBracket,
     RightCurlyBracket,
@@ -61,6 +62,127 @@ impl Display for Token{
     }
 }
 
+struct Tokenizer{
+    tokens: Vec<Token>
+}
+
+impl Tokenizer{
+    fn new() -> Tokenizer{
+        Tokenizer{
+            tokens: Vec::new()
+        }
+    }
+
+    fn tokenize(self:&mut Self, string: String){
+        self.tokens.clear();
+        let mut tokens  = Vec::new();
+
+        let chars: Vec<char> = string.chars().collect();
+
+        let mut i = 0;
+        while i < chars.len() {
+            let char = chars[i];
+            let token = match char {
+                '{' => Token::LeftCurlyBracket,
+                '}' => Token::RightCurlyBracket,
+                ',' => Token::Comma,
+                ':' => Token::Colon,
+                '[' => Token::LeftSquareBracket,
+                ']' => Token::RightSquareBracket,
+                '"' => {
+                    let mut string = String::new();
+                    i+=1;
+                    while i < chars.len() && chars[i] != '"' {
+                        string.push(chars[i]);
+                        i+=1;
+                    }
+                    Token::String(string)
+                }
+                't' => {
+                    let mut true_string = String::new();
+                    let mut j = i;
+                    while (j-i) < 4 {
+                        true_string.push(chars[j]);
+                        j+=1;
+                    }
+                    let token = if true_string == "true" {
+                        i = j;
+                        Token::True
+                    }else {
+                        Token::Empty
+                    };
+                    i-=1;
+                    token
+                }
+                'f' => {
+                    let mut false_string = String::new();
+                    let mut j = i;
+                    while (j-i) < 5 {
+                        false_string.push(chars[j]);
+                        j+=1;
+                    }
+                    let token = if false_string == "false" {
+                        i = j;
+                        Token::False
+                    }else {
+                        Token::Empty
+                    };
+                    i-=1;
+                    token
+                }
+                'n' => {
+                    let mut null_string = String::new();
+                    let mut j = i;
+                    while (j-i) < 4 {
+                        null_string.push(chars[j]);
+                        j+=1;
+                    }
+                    let token = if null_string == "null" {
+                        i = j;
+                        Token::Null
+                    }else {
+                        Token::Empty
+                    };
+                    i-=1;
+                    token
+                }
+                char => {
+                    let token = if char.is_numeric() {
+                        let mut string = String::new();
+                        while i < chars.len() && (chars[i].is_numeric() || chars[i] == '.') {
+                            string.push(chars[i]);
+                            i+=1;
+                        }
+                        i-=1;
+                        Token::Number(string.parse::<f64>().unwrap())
+                    } else {
+                        Token::Empty
+                    };
+                    token
+                }
+            };
+            match token {
+                Token::Empty => {
+                //     eat 5star do nothing
+                }
+                token => {
+                    tokens.push(token);
+                }
+            }
+            i += 1;
+        }
+
+        self.tokens = tokens;
+    }
+
+    pub fn display_tokens(&self) {
+        for token in &self.tokens {
+            print!("{}", token);
+        }
+    }
+}
+
+#[derive(Debug)]
 enum JsonValue{
     Null,
     Bool(bool),
@@ -70,104 +192,90 @@ enum JsonValue{
     Object(HashMap<String, JsonValue>)
 }
 
+struct Parser{
+    tokenizer:Tokenizer,
+}
+
+impl Parser {
+    fn new(string: String) -> Parser{
+        let mut tokenizer = Tokenizer::new();
+        tokenizer.tokenize(string);
+        Parser{
+            tokenizer
+        }
+    }
+
+    fn display_tokens(&self){
+        self.tokenizer.display_tokens();
+    }
+
+    pub fn parse(&self) -> JsonValue {
+        let mut iter = self.tokenizer.tokens.iter().peekable();
+        self.parse_value(&mut iter)
+    }
+
+    fn parse_value(&self, mut iter: &mut std::iter::Peekable<std::slice::Iter<Token>>) -> JsonValue{
+        match iter.peek().unwrap() {
+            Token::LeftCurlyBracket => {
+                self.parse_object(&mut iter)
+            }
+            Token::LeftSquareBracket => {
+                self.parse_array(&mut iter)
+            }
+            Token::String(string) => {
+                JsonValue::String(string.clone())
+            }
+            Token::Number(num) => {
+                JsonValue::Number(*num)
+            }
+            Token::True => {
+                JsonValue::Bool(true)
+            }
+            Token::False => {
+                JsonValue::Bool(false)
+            }
+            _ => {
+                JsonValue::Null
+            }
+        }
+    }
+
+    fn parse_array(&self, iter: &mut std::iter::Peekable<std::slice::Iter<Token>>) -> JsonValue{
+
+        let mut array = Vec::new();
+        iter.next(); // consume left square bracket
+
+        loop {
+            match iter.peek().unwrap() {
+                Token::RightSquareBracket => {
+                    iter.next();
+                    break;
+                }
+                Token::Comma => {}
+                value => {
+                    array.push(self.parse_value(iter));
+                    iter.next();
+                }
+            };
+        }
+        
+        JsonValue::Array(array)
+    }
+
+    fn parse_object(&self, iter: &mut std::iter::Peekable<std::slice::Iter<Token>>) -> JsonValue {
+        JsonValue::Null
+    }
+
+}
+
 
 fn main() {
     let json = fs::read_to_string("./test.json").unwrap();
 
-    let mut tokens = Vec::new();
+    let parser = Parser::new(json);
+    parser.display_tokens();
 
-    let chars: Vec<char> = json.chars().collect();
+    let json_value:JsonValue = parser.parse();
 
-    let mut i = 0;
-    while i < chars.len() {
-        let char = chars[i];
-        let token = match char {
-            '{' => Token::LeftCurlyBracket,
-            '}' => Token::RightCurlyBracket,
-            ',' => Token::Comma,
-            ':' => Token::Colon,
-            '[' => Token::LeftSquareBracket,
-            ']' => Token::RightSquareBracket,
-            '"' => {
-                let mut string = String::new();
-                i+=1;
-                while i < chars.len() && chars[i] != '"' {
-                    string.push(chars[i]);
-                    i+=1;
-                }
-                Token::String(string)
-            }
-            't' => {
-                let mut true_string = String::new();
-                let mut j = i;
-                while (j-i) < 4 {
-                    true_string.push(chars[j]);
-                    j+=1;
-                }
-                let token = if true_string == "true" {
-                    i = j;
-                    Token::True
-                }else {
-                    Token::Empty
-                };
-                i-=1;
-                token
-            }
-            'f' => {
-                let mut false_string = String::new();
-                let mut j = i;
-                while (j-i) < 5 {
-                    false_string.push(chars[j]);
-                    j+=1;
-                }
-                let token = if false_string == "false" {
-                    i = j;
-                    Token::False
-                }else {
-                    Token::Empty
-                };
-                i-=1;
-                token
-            }
-            'n' => {
-                let mut null_string = String::new();
-                let mut j = i;
-                while (j-i) < 4 {
-                    null_string.push(chars[j]);
-                    j+=1;
-                }
-                let token = if null_string == "null" {
-                    i = j;
-                    Token::Null
-                }else {
-                    Token::Empty
-                };
-                i-=1;
-                token
-            }
-            char => {
-                let token = if char.is_numeric() {
-                    let mut string = String::new();
-                    while i < chars.len() && (chars[i].is_numeric() || chars[i] == '.') {
-                        string.push(chars[i]);
-                        i+=1;
-                    }
-                    i-=1;
-                    Token::Number(string.parse::<f64>().unwrap())
-                } else {
-                    Token::Empty
-                };
-                token
-            }
-        };
-
-        tokens.push(token);
-        i += 1;
-    }
-
-    let tokens = tokens.iter().filter(|token: &&Token| if let Token::Empty = token { false } else { true } ).collect::<Vec<&Token>>();
-
-    for token in &tokens {
-        print!("{}", token);
-    }
+    print!("\n{:?}",json_value)
 }
